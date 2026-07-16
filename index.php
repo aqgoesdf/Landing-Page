449f6fa9fbde8fc9fe8f0889e2353cd2f0722811
aqgoesdf <aqgoesdf@gmail.com>
2026-07-16 13:06:42 -0300

    Edição da estrutura  Landing Page

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>AqGoes | Web Developer Full Stack</title>
+    <title>AqGoEs | Web Developer Full Stack</title>
     <!-- Tailwind CSS CDN -->
     <script src="https://cdn.tailwindcss.com"></script>
     <script>
